1895ef8d7e41c0fb703e827a6d8f91508f802a78
mohaShire12 <57040759+mohaShire12@users.noreply.github.com>
2023-09-11 13:13:05 +0300

    Update Data Cleaning Project .sql

--- Data Cleaning Portfolio Project Queries.sql
+++ Data Cleaning Portfolio Project Queries.sql
@@ -1,17 +1,5 @@
-/*
-
-Cleaning Data in SQL Queries
-
-*/
-
-
 Select *
 From PortfolioProject.dbo.NashvilleHousing
-
---------------------------------------------------------------------------------------------------------------------------
-
--- Standardize Date Format
-
 
 Select saleDateConverted, CONVERT(Date,SaleDate)
 From PortfolioProject.dbo.NashvilleHousing
@@ -19,23 +7,15 @@ From PortfolioProject.dbo.NashvilleHousing
 
 Update NashvilleHousing
 SET SaleDate = CONVERT(Date,SaleDate)
-
--- If it doesn't Update properly
-
+	
 ALTER TABLE NashvilleHousing
 Add SaleDateConverted Date;
 
 Update NashvilleHousing
 SET SaleDateConverted = CONVERT(Date,SaleDate)
 
-
- --------------------------------------------------------------------------------------------------------------------------
-
--- Populate Property Address data
-
 Select *
 From PortfolioProject.dbo.NashvilleHousing
---Where PropertyAddress is null
 order by ParcelID
 
 
@@ -55,14 +35,6 @@ JOIN PortfolioProject.dbo.NashvilleHousing b
 	on a.ParcelID = b.ParcelID
 	AND a.[UniqueID ] <> b.[UniqueID ]
 Where a.PropertyAddress is null
-
-
-
-
---------------------------------------------------------------------------------------------------------------------------
-
--- Breaking out Address into Individual Columns (Address, City, State)
-
 
 Select PropertyAddress
 From PortfolioProject.dbo.NashvilleHousing
@@ -139,18 +111,10 @@ From PortfolioProject.dbo.NashvilleHousing
 
 
 
-
---------------------------------------------------------------------------------------------------------------------------
-
-
--- Change Y and N to Yes and No in "Sold as Vacant" field
-
-
 Select Distinct(SoldAsVacant), Count(SoldAsVacant)
 From PortfolioProject.dbo.NashvilleHousing
 Group by SoldAsVacant
 order by 2
-
 
 
 
@@ -168,14 +132,6 @@ SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
 	   ELSE SoldAsVacant
 	   END
 
-
-
-
-
-
------------------------------------------------------------------------------------------------------------------------------------------------------------
-
--- Remove Duplicates
 
 WITH RowNumCTE AS(
 Select *,
@@ -205,83 +161,12 @@ From PortfolioProject.dbo.NashvilleHousing
 
 
 
----------------------------------------------------------------------------------------------------------
-
--- Delete Unused Columns
-
-
-
 Select *
 From PortfolioProject.dbo.NashvilleHousing
 
 
 ALTER TABLE PortfolioProject.dbo.NashvilleHousing
 DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------
-
---- Importing Data using OPENROWSET and BULK INSERT	
-
---  More advanced and looks cooler, but have to configure server appropriately to do correctly
---  Wanted to provide this in case you wanted to try it
-
-
---sp_configure 'show advanced options', 1;
---RECONFIGURE;
---GO
---sp_configure 'Ad Hoc Distributed Queries', 1;
---RECONFIGURE;
---GO
-
-
---USE PortfolioProject 
-
---GO 
-
---EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1 
-
---GO 
-
---EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1 
-
---GO 
-
-
----- Using BULK INSERT
-
---USE PortfolioProject;
---GO
---BULK INSERT nashvilleHousing FROM 'C:\Temp\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv'
---   WITH (
---      FIELDTERMINATOR = ',',
---      ROWTERMINATOR = '\n'
---);
---GO
-
-
----- Using OPENROWSET
---USE PortfolioProject;
---GO
---SELECT * INTO nashvilleHousing
---FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0',
---    'Excel 12.0; Database=C:\Users\alexf\OneDrive\Documents\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv', [Sheet1$]);
---GO
-
 
 
 
